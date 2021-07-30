a629c564c285211ae00ff47d8f8951b7f8ff96eb
Jack Bartlett <jacbart@gmail.com>
2021-07-29 21:09:43 -0700

    feat: made asdf.zsh excutable, add cargo path, git-cz alias

--- shell/env.zsh
+++ shell/env.zsh
@@ -15,10 +15,13 @@ export GOPATH="$HOME/.go"
 export GOROOT="/usr/local/go"
 export GOPRIVATE=github.com/journeyai
 
+# Cargo
+export CARGOPATH="$HOME/.cargo"
+
 # Export PATH
 export ISTIO_PATH="$HOME/workspace/istio-1.9.4/bin"
 export HOME_PATHS="$HOME/.local/bin:/$HOME/bin"
-export PATH="$PATH:$ITSIO_PATH:$HOME_PATHS:/snap/bin:$GOROOT/bin:$GOPATH/bin"
+export PATH="$PATH:$ITSIO_PATH:$HOME_PATHS:/snap/bin:$GOROOT/bin:$GOPATH/bin:$CARGOPATH/bin"
 
 # docker buildkit
 export DOCKER_BUILDKIT=1
